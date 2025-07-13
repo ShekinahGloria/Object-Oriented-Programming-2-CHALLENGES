8939a8a61bfd5e61b3dbb40fd19fb2983df16b68
SHEKINAH GLORIA <shekinah.otieno@strathmore.edu>
2025-07-14 00:48:26 +0300

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/Object-Oriented-Programming-2-CHALLENGES/ExceptionExamples/Challenge01/Greeter.java
+++
@@ -1,9 +0,0 @@
-public class Challenge01_Greeter {
-    void greet() {
-        System.out.println("Hi there");
-    }
-
-    public static void main(String[] args) {
-        (new Challenge01_Greeter()).greet();
-    }
-}
